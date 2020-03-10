1a22683b27a0a9575cf2bbdb1dc3e322fbd4a18d
Nathan Gonzalez <ncgonz@gmail.com>
2020-03-09 21:31:20 -0500

    composition

--- LegoMinifigures/Astronaut.cs
+++ LegoMinifigures/Astronaut.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using LegoMinifigures.Composition.Heads;
+using LegoMinifigures.Composition.Legs;
+using LegoMinifigures.Composition.Torsos;
 
 namespace LegoMinifigures
 {
@@ -9,22 +12,28 @@ namespace LegoMinifigures
     {
         //Public property
         public Location Location { get; set; }
-        
         // Expression Bodied Property (always readonly)
         public bool SuitedUp => Name != "Space Person";
-        
         // Read-Only Property
         public string Name { get; }
-
         // Public property with a private setter
         public string Job { get; private set; }
-        
         public int OxygenLevel { private get; set; }
 
-        public Astronaut(string name, string job)
+        public AstronautTorso Torso { get; set; }
+        public AstronautLegs Legs { get; set; }
+        public ZoeHead Head { get; set; }
+
+        public Astronaut(string name, string job, 
+                         ZoeHead head, AstronautTorso torso, 
+                         AstronautLegs legs)
         {
             Name = name;
             Job = job;
+
+            Head = head;
+            Torso = torso;
+            Legs = legs;
         }
 
         public void Promote()
@@ -32,9 +41,13 @@ namespace LegoMinifigures
             Job = "Commander of Janitors";
         }
 
-        public void DoYourJob()
+        public void DoYourJob(int stepsToWalk)
         {
             Console.WriteLine($"{Name} is doing all their {Job} duties...");
+            Legs.Walk(stepsToWalk);
+            Head.EatPie("Cherry");
+            Torso.Flex();
+            Legs.Walk(stepsToWalk);
         }
 
 
---
+++ LegoMinifigures/Composition/Heads/ZoeHead.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LegoMinifigures.Composition.Heads
+{
+    class ZoeHead
+    {
+        public bool Helmeted { get;set; }
+        public string FacialExpression { get; set; }
+        public LegoColor Color { get;set; }
+        public LegoColor EyeColor {get; set; }
+
+        public void Talk()
+        {
+            Console.WriteLine($"Zoe says 'Tell my wife I love her very much.' while {FacialExpression} emphatically ");
+        }
+
+        public void EatPie(string typeOfPie)
+        {
+            if (typeOfPie =="apple")
+                Console.WriteLine("MMM.. Pie");
+            else
+                Console.WriteLine("This is not pie.");
+        }
+    }
+
+    enum LegoColor
+    {
+        Yellow,
+        Red,
+        Green,
+        White,
+        Grey,
+        Blue,
+        Black,
+        Orange
+    }
+}
